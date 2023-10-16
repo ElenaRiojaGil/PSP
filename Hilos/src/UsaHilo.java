548ba76ecc8f19ec65ec775779f40f3cdb9b5423
ElenaRiojaGil <eriojita@gmail.com>
2023-10-16 14:02:34 +0200

    Hilos

---
+++ Hilos/src/UsaHilo.java
@@ -0,0 +1,16 @@
+public class UsaHilo extends Thread {
+    public static void main(String[] args) {
+        UsaHilo mihilo= new UsaHilo();
+        mihilo.start();
+
+        for (int i=0;i<5;i++){
+            System.out.println("Estoy fuera del hilo... "+i);
+        }
+
+    }
+    public void run(){
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Estoy en el hilo...." +i);
+        }
+    }
+}
